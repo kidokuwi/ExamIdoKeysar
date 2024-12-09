ffc749a21cb8406b01ed9f5fd7b1159fe6fbb4d4
ido <handasvs@gmail.com>
2024-12-09 08:55:54 +0200

    a

---
+++ ExamIdoKeysar/Pages/monster.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExamIdoKeysar.Pages
+{
+    public class monsterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
