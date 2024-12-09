b67bc374a461f7723997852d50e7e9cf66fdd6c7
ido <handasvs@gmail.com>
2024-12-09 09:52:34 +0200

    c

---
+++ ExamIdoKeysar/Properties/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[itemsTB]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [producer] NCHAR(20) NOT NULL, 
+    [itemName] NCHAR(20) NOT NULL, 
+    [calories] INT NOT NULL, 
+    [teaSpoons] INT NOT NULL
+)
